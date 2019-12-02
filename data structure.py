e1f53fcfccf435b13b43a9636caabfb377b579d2
harshkhurana6 <56379464+harshkhurana6@users.noreply.github.com>
2019-12-02 19:32:22 +0530

    Added Linked list program with python

--- data structure.py
+++ data structure.py
@@ -529,4 +529,29 @@ if __name__ == '__main__':
     while not myQueue.isEmpty(): 
         print(myQueue.delete()) 
 
+class node:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+
+class linkedlist:
+    def __init__(self):
+        self.head=None
+
+    def printlist(self):
+        temp=self.head
+        while(temp):
+            print(temp.data)
+            temp=temp.next
+
+if __name__=='__main__':
+    llist=linkedlist()
+
+    llist.head=node(1)
+    second=node(2)
+    third=node(3)
+
+    llist.head.next=second
+    second.next=third
+    llist.printlist()
 
